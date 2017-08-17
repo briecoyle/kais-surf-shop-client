309566a80fe3f5d049d204abe4c728d68c4446d3
briecoyle <brie.coyle@gmail.com>
2017-08-16 22:01:03 -0400

    Add Redux state management

---
+++ src/actions/surfboardForm.js
@@ -0,0 +1,14 @@
+export const updateSurfboardFormData = surfboardFormData => {
+  return {
+    type: "UPDATED_DATA",
+    surfboardFormData
+  }
+}
+
+
+export const resetSurfboardForm = () => {
+  return {
+    type: 'RESET_SURFBOARD_FORM',
+
+  }
+}
--- src/containers/Surfboards.js
+++ src/containers/Surfboards.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import SurfboardCard from '../components/SurfboardCard';
+import SurfboardForm from './SurfboardForm';
+import { getSurfboards } from '../actions/surfboards';
 import './Surfboards.css';
 
-const Surfboards = (props) =>(
-  <div className="SurfboardContainer">
-    <h1>Surfboards</h1>
-    {props.surfboards.map(surfboard => (
-      <div key={surfboard.id} className="SurfboardCard">
-        <h3>{surfboard.name}</h3>
-        <p>Price: ${surfboard.price}</p>
-        <img className="SurfboardImage" src={surfboard.img_url} alt={surfboard.name}/>
-        <p>Length: {surfboard.length} inches</p>
+class Surfboards extends Component {
+
+  componentDidMount() {
+    this.props.getSurfboards()
+  }
+
+  render() {
+    return (
+      <div className="SurfboardContainer">
+        <h1>Surfboards</h1>
+        {this.props.surfboards.map(surfboard => <SurfboardCard key={surfboard.id} surfboard={surfboard} />)}
+        <SurfboardForm />
       </div>
     )
-  )}
-  </div>
-);
+  }
+}
 
-export default Surfboards;
+const mapStateToProps = (state) => {
+  return ({
+    surfboards: state.surfboards
+  })
+}
+
+export default connect(mapStateToProps, { getSurfboards })(Surfboards);
